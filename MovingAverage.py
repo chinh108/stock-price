cc37be27bda80b2227f7994b633126143f267408
chinh108 <57782333+chinh108@users.noreply.github.com>
2021-01-09 10:46:35 +0700

    Update MovingAverage.py

--- MovingAverage.py
+++ MovingAverage.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 from fastai.tabular.all import add_datepart
 from sklearn.linear_model import LinearRegression
 
-# reading the data
 df = pd.read_csv('NSE-TATAGLOBAL11.csv')
 
 # looking at the first five rows of the data
@@ -52,58 +51,9 @@ rms=np.sqrt(np.mean(np.power((np.array(valid['Close'])-preds),2)))
 print('\n RMSE value on validation set:')
 print(rms)
 
-
-#setting index as date values
-df['Date'] = pd.to_datetime(df.Date,format='%Y-%m-%d')
-df.index = df['Date']
-
-#sorting
-data = df.sort_index(ascending=True, axis=0)
-
-#creating a separate dataset
-new_data = pd.DataFrame(index=range(0,len(df)),columns=['Date', 'Close'])
-
-for i in range(0,len(data)):
-    new_data['Date'][i] = data['Date'][i]
-    new_data['Close'][i] = data['Close'][i]
-
-#create features
-add_datepart(new_data, 'Date')
-new_data.drop('Elapsed', axis=1, inplace=True)  #elapsed will be the time stamp
-
-new_data['mon_fri'] = 0
-for i in range(0,len(new_data)):
-    if (new_data['Dayofweek'][i] == 0 or new_data['Dayofweek'][i] == 4):
-        new_data['mon_fri'][i] = 1
-    else:
-        new_data['mon_fri'][i] = 0
-
-#split into train and validation
-train = new_data[:987]
-valid = new_data[987:]
-
-x_train = train.drop('Close', axis=1)
-y_train = train['Close']
-x_valid = valid.drop('Close', axis=1)
-y_valid = valid['Close']
-
-#implement linear regression
-model = LinearRegression()
-model.fit(x_train,y_train)
-
-#make predictions and find the rmse
-preds = model.predict(x_valid)
-rms=np.sqrt(np.mean(np.power((np.array(y_valid)-np.array(preds)),2)))
-rms
-
 #plot
-plt.plot(train['Close'])
-plt.plot(valid[['Close', 'Predictions']])
-
 valid['Predictions'] = 0
 valid['Predictions'] = preds
-
-valid.index = new_data[987:].index
-train.index = new_data[:987].index
-
+plt.plot(train['Close'])
+plt.plot(valid[['Close', 'Predictions']])
 plt.show()
